26c13122f430f030865372e668c65a4a6f8dc866
mjkim41 <mj10283@naver.com>
2025-04-14 18:09:57 +0900

    [setup/build] add deploy configuration - delete unnecessary codes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/' // username.github.io 레포지토리는 루트 경로 사용
 })
